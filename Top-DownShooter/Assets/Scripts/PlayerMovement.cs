70b137420a19045f22424d71097dbc24a83c403c
Jaype27 <jaypee.0927@gmail.com>
2020-09-17 14:04:23 -0400

    movement and rotation player

---
+++ Top-DownShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float _forwardSpeed;
+	public float _reverseSpeed;
+	public float _rotationSpeed;
+	private float _input;
+	private Rigidbody2D _rb2d;
+
+	void Awake() {
+		_rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShipRotation();
+		ShipMovement();
+	}
+
+
+
+	void ShipMovement() {
+		
+		if(Input.GetKey(KeyCode.W)) {
+			_input = 1;
+			_rb2d.velocity = transform.up * _forwardSpeed * _input;
+		}
+
+		if(Input.GetKey(KeyCode.S)) {
+			_input = -1;
+			_rb2d.velocity = transform.up * _reverseSpeed * _input;
+		}
+	}
+	
+	void ShipRotation() {
+		_input = Input.GetAxisRaw("Horizontal");
+		transform.Rotate(0, 0, -_rotationSpeed * _input);
+	}
+}

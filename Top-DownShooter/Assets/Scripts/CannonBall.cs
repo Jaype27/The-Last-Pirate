c09d2e0c339e1868647019b6b11aadf6ca96f220
Jaype27 <jaypee.0927@gmail.com>
2020-09-18 01:19:10 -0400

    shooting left cannon, firerate, cannonball velocity

---
+++ Top-DownShooter/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour {
+
+	public float _speed;
+	private Rigidbody2D rb2d;
+	public float _activeSpan;
+	private float _activeTimer;
+
+	void Awake() {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		_activeTimer = _activeSpan;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb2d.velocity = transform.up * _speed;
+
+		_activeTimer -= Time.deltaTime;
+		if(_activeTimer <= 0f) {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
